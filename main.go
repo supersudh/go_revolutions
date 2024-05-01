0660f75745eb20888e6138cbb3b42dd9422e5eb9
Sudharsan Ravikumar <supersudh@gmail.com>
2024-05-01 20:14:59 +0100

    [Multiline printing]

--- main.go
+++ main.go
@@ -7,9 +7,8 @@ package main
 import "fmt"
 
 func main() {
-	const name = "Sudharsan Ravikumar"
-	const age = 24
-
-	fmt.Printf("%s is %d years old \n", name, age)
-	fmt.Printf("%s is %d years old. \t and the type is %T and %T\n", name, age, name, age)
+	fmt.Println(`
+		Space
+		is in Space
+	`)
 }
