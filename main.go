6a577b9072cd210de7541faac7a057bf7870bfcd
Sudharsan Ravikumar <supersudh@gmail.com>
2024-05-01 20:01:09 +0100

    [Println] warning

---
+++ main.go
@@ -0,0 +1,12 @@
+/*
+  [TEST COMMENTS]
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	const name, age = "Sudharsan", 0024
+	fmt.Println("%s is %d years old.\n", name, age) // TODO: [supersudh] fmt.Println call has possible Printf formatting directive %sprintfdefault
+}
